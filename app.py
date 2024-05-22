dc1556280b6989158599d301b05f23d03d5daff5
EricWithC04 <negativegame117@gmail.com>
2024-05-22 11:06:29 -0300

    feat: Validations

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
 import os
+from requests.exceptions import HTTPError, Timeout, ConnectionError
 import requests
 from bs4 import BeautifulSoup
 
-# crear carpetas si no existen
-if not os.path.exists('images'):
-    os.makedirs('images')
+def download_images(url):
+    try:
+        # crear carpetas si no existen
+        if not os.path.exists('images'):
+            os.makedirs('images')
 
-url = 'https://www.google.com/search?sca_esv=8b3c2fe050857b40&sxsrf=ADLYWILNxvLDeiVg4-QhhNAhiwdMHlQgOw:1716381846051&q=burros&tbm=isch&source=lnms&sa=X&ved=2ahUKEwij8-3RpKGGAxWxu5UCHbTVBJUQ0pQJegQIDRAB&biw=1366&bih=647&dpr=1'
+        # realizamos la petición y pasarle el texto plano a BeautifulSoup
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-# realizamos la petición y pasarle el texto plano a BeautifulSoup
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+        # traemos todas las etiquetas img.
+        results = soup.find_all('img')
 
-# traemos todas las etiquetas img.
-results = soup.find_all('img')
+        # filtrar los resultados
+        results = [img for img in results if img["src"].startswith("https://")]
 
-# filtrar los resultados
-results = [img for img in results if img["src"].startswith("https://")]
+        # definimos las extensiones permitidas
+        permited_extensions = {
+            'image/jpeg',
+            'image/png',
+            'image/webp',
+        }
 
-# definimos las extensiones permitidas
-permited_extensions = {
-    'image/jpeg',
-    'image/png',
-    'image/webp',
-}
+        # se descargan cada una de las imagenes
+        for i, img in enumerate(results, start=1):
+            filename = f'Imagen-{i}.jpg'
+            result_image = requests.get(img["src"])
 
-# se descargan cada una de las imagenes
-for i, img in enumerate(results, start=1):
-    filename = f'Imagen-{i}.jpg'
-    result_image = requests.get(img["src"])
+            # comprobar que la imagen tiene el formato permitido
+            if (result_image.headers.get('content-type') in permited_extensions):
+                with open(f'images/{filename}', 'wb') as file:
+                    file.write(result_image.content)
 
-    # comprobar que la imagen tiene el formato permitido
-    if (result_image.headers.get('content-type') in permited_extensions):
-        with open(f'images/{filename}', 'wb') as file:
-            file.write(result_image.content)
+        print("Imagenes descargadas exitosamente")
+    except HTTPError as http_err:
+        print(f"Error en la petición HTTP: {http_err}")
+    except Timeout as timeout_err:
+        print(f"Error, se ha agotado el tiempo de espera: {timeout_err}")
+    except ConnectionError as conn_err:
+        print(f"Error, comprueba tu conexión y vuelve a intenter: {conn_err}")
 
-print("Imagenes descargadas exitosamente")
+download_images('https://www.google.com/search?sca_esv=8b3c2fe050857b40&sxsrf=ADLYWILNxvLDeiVg4-QhhNAhiwdMHlQgOw:1716381846051&q=burros&tbm=isch&source=lnms&sa=X&ved=2ahUKEwij8-3RpKGGAxWxu5UCHbTVBJUQ0pQJegQIDRAB&biw=1366&bih=647&dpr=1')
